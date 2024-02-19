0398c964bfd143bc753e56524c62225164eb7ffb
Amrein Milán <amrein7@gmail.com>
2024-02-19 15:51:09 +0100

    Added Spell Globe widgets to Overlay with Abilities bound to them

---
+++ Source/Aura/AuraLogChannels.h
@@ -0,0 +1,7 @@
+#pragma once	// making sure we never include/define a header file/variable more than once if it's already been included/defined
+
+#include "CoreMinimal.h"
+#include "Logging/LogMacros.h"
+
+// Declare custom Log Channel
+DECLARE_LOG_CATEGORY_EXTERN(LogAura, Log, All);
